a53979140f39b85d9dc72fb0ee50d3fe3c115693
KaranBastola84 <np05cp4a230154@iic.edu.np>
2026-02-03 13:12:12 +0545

    Set up application with eSewa service registration and CORS configuration

---
+++ Program.cs
@@ -0,0 +1,41 @@
+using EsewaBackend.Services;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+// Configure HttpClient
+builder.Services.AddHttpClient();
+
+// Register eSewa service
+builder.Services.AddScoped<IEsewaService, EsewaService>();
+
+// Configure CORS
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll", policy =>
+    {
+        policy.AllowAnyOrigin()
+              .AllowAnyMethod()
+              .AllowAnyHeader();
+    });
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+app.UseCors("AllowAll");
+app.UseAuthorization();
+app.MapControllers();
+
+app.Run();
